ceded84d47b1fe6dfcf478b675dfc54447efba5f
Ivan Protopopov <ewanprotopopov@yandex.ru>
2024-11-26 17:14:12 +0300

    fix gif

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,7 @@ android {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+        resources.excludes.add("META-INF/versions/9/OSGI-INF/MANIFEST.MF")
     }
 }
 
@@ -59,6 +57,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.identity.jvm)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/vk_homework_2/RequestController.kt
+++ app/src/main/java/com/example/vk_homework_2/RequestController.kt
@@ -1,10 +1,21 @@
 package com.example.vk_homework_2
 
 
-data class GifResponse(val data: GifData)
-data class GifData(val images: GifImages)
-data class GifImages(val original: GifOriginal)
-data class GifOriginal(val url: String)
+data class GifResponse(
+    val data: GifData
+)
+
+data class GifData(
+    val images: GifImages
+)
+
+data class GifImages(
+    val original: GifOriginal
+)
+
+data class GifOriginal(
+    val url: String
+)
 
 
 interface RequestController {
